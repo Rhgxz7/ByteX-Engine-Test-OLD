89b0f8efcee19698c8c4308321da36c11b980266
Rhgxz7 <105667149+Rhgxz7@users.noreply.github.com>
2022-10-18 18:27:42 +0100

    finished byx_VBO class

---
+++ src/byx_VBO.cpp
@@ -0,0 +1,24 @@
+#include"byx_VBO.hpp"
+
+using namespace byx;
+
+VBO::VBO(GLfloat* vertices, GLsizeiptr size) {
+	glGenBuffers(1, &ID);
+	glBindBuffer(GL_ARRAY_BUFFER, ID);
+	glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+}
+
+
+void VBO::bind() {
+	glBindBuffer(GL_ARRAY_BUFFER, ID);
+}
+
+
+void VBO::unbind() {
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+
+void VBO::destroy() {
+	glDeleteBuffers(1, &ID);
+}

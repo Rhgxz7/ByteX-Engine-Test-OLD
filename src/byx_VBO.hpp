6144b99ddb668c7cd02bc763f3d40847fb3e9ddd
Rhgxz7 <105667149+Rhgxz7@users.noreply.github.com>
2022-10-18 18:24:33 +0100

    Create byx_VBO.hpp

---
+++ src/byx_VBO.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <GL/glew.h>
+
+namespace byx {
+
+    class VBO {
+        public:
+
+	    GLuint ID;
+	
+	    VBO(GLfloat* vertices, GLsizeiptr size);
+
+	
+	    void bind();
+	    void unbind();
+	    void Destroy();
+};
+
+}

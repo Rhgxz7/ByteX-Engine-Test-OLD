54337aa04ded8783ecc8b09a94c735de839124ce
Rhgxz7 <105667149+Rhgxz7@users.noreply.github.com>
2022-10-18 18:42:57 +0100

    Update byx_VAO.hpp

--- src/byx_VAO.hpp
+++ src/byx_VAO.hpp
@@ -1,1 +1,24 @@
-//byx_VAO.hpp
+#pragma once
+
+#include <GL/glew.h>
+#include "byx_VBO.hpp"
+
+namespace byx {
+
+    class VAO
+    {
+        public:
+        
+            GLuint ID;
+        
+            VAO();
+
+        
+            void linkVBO(VBO& VBO, GLuint layout);
+        
+            void bind();
+            void unbind();
+            void destroy();
+    };
+
+}

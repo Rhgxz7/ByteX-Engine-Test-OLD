20aede0d7429bb0c181d18f61b4a73e432a8eb18
Rhgxz7 <105667149+Rhgxz7@users.noreply.github.com>
2022-10-18 17:11:58 +0100

    Update sanbox.cpp

--- src/sanbox.cpp
+++ src/sanbox.cpp
@@ -53,10 +53,20 @@ namespace byx {
         glDeleteShader(vertexShader);
         glDeleteShader(fragmentShader);
 
-        GLuint VBO;
-        glGenBuffers(1, &VBO); 
+        GLuint VAO, VBO;
 
+        glGenVertexArrays(1, &VAO);
+
+        glBindVertexArray(VAO);
+
+        glGenBuffers(1, &VBO); 
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
 
 
         glClearColor(0.50f, 0.13f, 0.17f, 1.0f);

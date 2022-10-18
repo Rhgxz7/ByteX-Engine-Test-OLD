bd9bb2d740819e14aaddf1a5b02b72ea3052add5
Rhgxz7 <105667149+Rhgxz7@users.noreply.github.com>
2022-10-18 17:46:15 +0100

    Created shader files and removed shaders from sandbox.cpp

--- src/sanbox.cpp
+++ src/sanbox.cpp
@@ -2,20 +2,7 @@
 #include <math.h>
 
 // Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
 
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.0f, 0.2f, 0.7f, 1.0f);\n"
-"}\n\0";
 
 namespace byx {
 

0d4d25a555809980cd3be039e7ac8dfbcfc2753e
Rhgxz7 <105667149+Rhgxz7@users.noreply.github.com>
2022-10-18 17:22:53 +0100

    Changed Frag color

--- src/sanbox.cpp
+++ src/sanbox.cpp
@@ -8,12 +8,13 @@ const char* vertexShaderSource = "#version 330 core\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
+
 //Fragment Shader source code
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"   FragColor = vec4(0.0f, 0.2f, 0.7f, 1.0f);\n"
 "}\n\0";
 
 namespace byx {
